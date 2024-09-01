4cb3654ddb4a3413f8235254284a24d753d20008
Alina Alieks <alieksieienko.alina@gmail.com>
2024-09-02 02:12:26 +0300

    load more

--- src/components/TrucksList/TrucksList.jsx
+++ src/components/TrucksList/TrucksList.jsx
@@ -3,17 +3,32 @@ import { selectFilteredCampers } from "../../redux/campers/selectors";
 import TruckItem from "../TruckItem/TruckItem";
 
 import css from "./TrucksList.module.css";
+import { useState } from "react";
 
 export default function TrucksList() {
+  const [visibleCount, setVisibleCount] = useState(4);
+
+  const handleLoadMore = () => {
+    setVisibleCount((prevCount) => prevCount + 4);
+  };
+
   const campers = useSelector(selectFilteredCampers);
   console.log(campers);
+
   return (
-    <ul className={css.list}>
-      {campers.map((item) => (
-        <li className={css.listItem} key={item.id}>
-          <TruckItem camper={item} />
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul className={css.list}>
+        {campers.slice(0, visibleCount).map((item) => (
+          <li className={css.listItem} key={item.id}>
+            <TruckItem camper={item} />
+          </li>
+        ))}
+      </ul>
+      {visibleCount < campers.length && (
+        <button className={css.button} onClick={handleLoadMore} type="button">
+          Load more
+        </button>
+      )}
+    </div>
   );
 }
