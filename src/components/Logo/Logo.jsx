d7e5b65076321504ed133c3dd01bc20239ae0b9c
Alina Alieks <alieksieienko.alina@gmail.com>
2024-09-01 13:25:34 +0300

    logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import css from "./Logo.module.css";
 export default function Logo() {
   return (
     <Link to="/" className={css.logoLink}>
-      <img src="/public/logo.svg" alt="Logo" />
+      <img src="../../public/logo.svg" alt="Logo" />
     </Link>
   );
 }
